000df90e4cd21e10fc217633d2edaaf34eb0722c
Marco Willems (M17.1) <willima07@gmail.com>
2015-01-27 00:13:12 +0100

    Frames!

--- JRPGMaker/src/be/willima/jrpgmaker/Frame.java
+++ JRPGMaker/src/be/willima/jrpgmaker/Frame.java
@@ -7,12 +7,27 @@ package be.willima.jrpgmaker;
 
 import be.willima.jrpgdatabase.JRPGDao;
 import be.willima.jrpgdatabase.JRPGDatabaseFactory;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
 
 /**
  *
  * @author marco
  */
-class Frame {
+class Frame extends JFrame {
+    
+    private JRPGDao dao;
+
+    public Frame(JRPGDao dao) throws HeadlessException {
+        super("JRPGMaker");
+        this.dao = dao;
+        
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(new Dimension(400, 300));
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+    }
 
     /**
      * @param args the command line arguments
@@ -20,7 +35,10 @@ class Frame {
     public static void main(String[] args) {
         // TODO code application logic here
         JRPGDao dao = JRPGDatabaseFactory.getJRPGDao(JRPGDatabaseFactory.DaoType.DUMMY);
-        System.out.println(dao.getInfo());
+        System.out.println("Dao: " + dao.getInfo());
+        
+        Frame frame = new Frame(dao);
+        frame.setVisible(true);
     }
     
 }
