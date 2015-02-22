cc69ef2ed892a7993bc9f52c961fce21d9aeef06
Marco Willems <marco.willems@ugent>
2015-02-22 12:59:18 +0100

    Made raster transparent over tiles instead of in between tiles.

--- JRPGMaker/src/be/willima/jrpgmaker/mapbuilder/view/MapPanel.java
+++ JRPGMaker/src/be/willima/jrpgmaker/mapbuilder/view/MapPanel.java
@@ -6,6 +6,7 @@
 package be.willima.jrpgmaker.mapbuilder.view;
 
 import be.willima.jrpgdatabase.model.JRPGMap;
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -13,9 +14,11 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 /**
+ * TODO comments
  *
  * @author marco
  */
@@ -67,60 +70,40 @@ public class MapPanel extends JPanel {
         int width = map == null ? 0 : map.getWidth();
         int height = map == null ? 0 : map.getHeight();
         int scaledTileSize = JRPGMap.TILE_SIZE * scale;
-        if (showRaster) {
-            int dimWidth = width * scaledTileSize + width + 1;
-            int dimHeight = height * scaledTileSize + height + 1;
-            this.setSize(new Dimension(dimWidth, dimHeight));
-            this.setPreferredSize(new Dimension(dimWidth, dimHeight));
-            this.setMinimumSize(new Dimension(dimWidth, dimHeight));
-            this.setMaximumSize(new Dimension(dimWidth, dimHeight));
-        } else {
-            int dimWidth = width * scaledTileSize;
-            int dimHeight = height * scaledTileSize;
-            this.setSize(new Dimension(dimWidth, dimHeight));
-            this.setPreferredSize(new Dimension(dimWidth, dimHeight));
-            this.setMinimumSize(new Dimension(dimWidth, dimHeight));
-            this.setMaximumSize(new Dimension(dimWidth, dimHeight));
-        }
+        int dimWidth = width * scaledTileSize;
+        int dimHeight = height * scaledTileSize;
+        this.setSize(new Dimension(dimWidth, dimHeight));
+        this.setPreferredSize(new Dimension(dimWidth, dimHeight));
+        this.setMinimumSize(new Dimension(dimWidth, dimHeight));
+        this.setMaximumSize(new Dimension(dimWidth, dimHeight));
+        
+        this.repaint();
     }
 
     private Point getTilePixelPoint(int tileX, int tileY) {
         int scaledTileSize = JRPGMap.TILE_SIZE * scale;
-        int x, y;
-        if (showRaster) {
-            x = tileX * (scaledTileSize + 1) + 1;
-            y = tileY * (scaledTileSize + 1) + 1;
-        } else {
-            x = tileX * scaledTileSize;
-            y = tileY * scaledTileSize;
-        }
+        int x = tileX * scaledTileSize;
+        int y = tileY * scaledTileSize;
+        
         return new Point(x, y);
     }
 
     private Point getMapPoint(int tilePixelX, int tilePixelY) {
         int scaledTileSize = JRPGMap.TILE_SIZE * scale;
-        int x, y;
-        if (showRaster) {
-            int tileJump = scaledTileSize + 1;
-            x = tilePixelX / tileJump;
-            y = tilePixelY / tileJump;
-            if (tilePixelX % tileJump == 0) {
-                x = -1;
-            }
-            if (tilePixelY % tileJump == 0) {
-                y = -1;
-            }
-        } else {
-            x = tilePixelX / scaledTileSize;
-            y = tilePixelY / scaledTileSize;
-        }
+        int x = tilePixelX / scaledTileSize;
+        int y = tilePixelY / scaledTileSize;
+        
         return new Point(x, y);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
+//        Graphics2D g2 = (Graphics2D) g;
+        
+        // Double buffering ...
+        BufferedImage buff = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = buff.createGraphics();
 
         g2.setColor(Color.red);
         g2.fillRect(0, 0, getWidth(), getHeight()); // Background for testing
@@ -135,15 +118,23 @@ public class MapPanel extends JPanel {
                 }
             }
             if (showRaster) { // Draw raster
-                g2.setColor(new Color(0xff00aadd));
-                for (int x = 0; x < getWidth(); x += scaledTileSize + 1) { // Vertical lines
-                    g2.drawLine(x, 0, x, getHeight() - 1);
+//                g2.setColor(new Color(0x7700aadd));
+                g2.setColor(new Color(0x666666));
+                g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f));
+
+                for (int x = 0; x <= getWidth(); x += scaledTileSize) { // Vertical lines
+                    g2.drawLine(x - 1, 0, x - 1, getHeight());
+                    g2.drawLine(x, 0, x, getHeight());
                 }
-                for (int y = 0; y < getHeight(); y += scaledTileSize + 1) { // Horizontal lines
-                    g2.drawLine(0, y, getWidth() - 1, y);
+                for (int y = 0; y <= getHeight(); y += scaledTileSize) { // Horizontal lines
+                    g2.drawLine(0, y - 1, getWidth(), y - 1);
+                    g2.drawLine(0, y, getWidth(), y);
                 }
             }
         }
+        
+        g.drawImage(buff, 0, 0, null);
+        g2.dispose();
     }
 
     private class MapMouseInputHandler extends MouseAdapter {
@@ -151,11 +142,7 @@ public class MapPanel extends JPanel {
         @Override
         public void mouseClicked(MouseEvent e) {
             Point p = getMapPoint(e.getX(), e.getY());
-            if (p.x == -1 || p.y == -1) {
-                System.out.println("Mouse clicked on raster.");
-            } else {
-                System.out.println("Mouse clicked on tile [" + p.x + ", " + p.y + "].");
-            }
+            System.out.println("Mouse clicked on tile [" + p.x + ", " + p.y + "].");
         }
 
     }
